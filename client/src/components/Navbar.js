16d7062d27fe97a30da54445602dd56aef730335
unknown <harshitsinha688.com>
2026-07-01 14:23:44 +0530

    updated Navabar UI  logo & links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,42 +1,116 @@
+
 import { Link, useNavigate } from "react-router-dom";
+import logo from "../assets/logo2.png";
 
 function Navbar() {
-  const navigate = useNavigate();
+   const navigate = useNavigate();
+   const handleLogout = () => {
+     localStorage.clear();
+     navigate("/");
+   };
 
-  const handleLogout = () => {
-    localStorage.clear();
-    navigate("/");
+      const handleProtectedNavigation = (path) => {
+
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      alert("Please login first to access this page.");
+      navigate("/login");
+      return;
+    }
+
+    navigate(path);
   };
-
   return (
-    <div className="bg-gray-900 text-white px-6 py-4 flex justify-between items-center shadow-md">
+    <nav className="bg-gray-900 text-white shadow-sm border-b border-gray-200 px-4 md:px-8 py-3">
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
 
-      <h1 className="text-xl font-bold text-blue-400">
-        AI Interview Coach
-      </h1>
-      
-      <div className="flex gap-6 items-center">
-
-        <Link to="/dashboard" className="hover:text-blue-400">
-          Home
-        </Link>
-
-        <Link to="/my-interviews" className="hover:text-blue-400">
-          My Interviews
-        </Link>
-        <Link to="/profile" className="hover:text-blue-400">
-          Profile
-        </Link>
-
-        <button
-          onClick={handleLogout}
-          className="bg-red-500 px-3 py-2 rounded-lg hover:bg-red-600"
+        {/* Logo Section */}
+        <Link
+         onClick={() => handleProtectedNavigation("/dashboard")}
+          to="/dashboard"
+          className="flex items-center gap-2 md:gap-3"
         >
-          Logout
-        </button>
+          <img
+            src={logo}
+            alt="AI Coach Logo"
+            className="
+              h-8
+              w-auto
+              md:h-10
+              object-contain
+            "
+          />
 
+          
+        </Link>
+
+        {/* Right Side Menu */}
+        <div className="flex items-center gap-3 md:gap-6">
+
+          <Link
+           onClick={() => handleProtectedNavigation("/dashboard")}
+            to="/dashboard"
+            className="
+              text-blue-600
+              hover:text-white
+              text-sm
+              md:text-base
+              font-medium
+              transition
+            "
+          >
+            Dashboard
+          </Link>
+
+          <Link
+           onClick={() => handleProtectedNavigation("/my-interviews")}
+            to="/my-interviews"
+            className="
+              text-blue-600
+              hover:text-white
+              text-sm
+              md:text-base
+              font-medium
+              transition
+            "
+          >
+            Interviews
+          </Link>
+
+          <Link
+          onClick={() => handleProtectedNavigation("/profile")}
+            to="/profile"
+            className="
+              text-blue-600
+              hover:text-white
+              text-sm
+              md:text-base
+              font-medium
+              transition
+            "
+          >
+            Profile
+          </Link>
+
+        </div>
+       {localStorage.getItem("token") && ( 
+      <button
+            onClick={handleLogout}
+            className= "text-blue-600
+              hover:text-white
+              text-sm
+              md:text-base
+              font-medium
+              transition"
+          >
+            Logout
+          </button>)
+}
+
+        
       </div>
-    </div>
+    </nav>
   );
 }
 
